654da391d6436a8f9529af10b0e3b5b6024bad73
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-07-23 16:25:48 +0530

    Update twilioClient.js

--- utils/twilioClient.js
+++ utils/twilioClient.js
@@ -1,6 +1,6 @@
 // utils/twilioClient.js
 
-import twilio from 'twilio';
+const twilio = require('twilio');
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
